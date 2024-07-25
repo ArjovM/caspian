9b93c96d9f0faf3e342c7c27b839467c4487c369
Arjov Maharjan <arjov.maharjan5@gmail.com>
2024-07-25 19:10:31 +0545

    ebooks and login success

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -39,7 +39,7 @@ const LoginForm = () => {
         </div>
       ) : (
         <div className="login-form">
-          <h1>Login</h1>
+          <h1>Login to visit the Site</h1>
           <form onSubmit={handleLogin}>
             <input
               type="text"
